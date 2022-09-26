0afc15158e2f54405227de259b5bcd197e21daf3
kty023 <68367651+kty023@users.noreply.github.com>
2022-09-26 15:05:42 +0900

    Avoid Image optimization Error

--- next.config.js
+++ next.config.js
@@ -5,17 +5,41 @@ const nextConfig = {
 
 const debug = process.env.NODE_ENV !== "production";
 
+// const withOptimizedImages = require('next-optimized-images');
+
+// module.exports = withOptimizedImages({
+//   basePath: process.env.GITHUB_ACTIONS && "/nextjs-deploy-github-pages-test",
+//   trailingSlash: true,
+// //  assetPrefix: !debug ? "/notes/" : "",
+// //  images: { loader: "custom" },
+//   // experimental: {
+//   //   images: {
+//   //       unoptimized: true,
+//   //   },
+//   // },
+//   // images: {
+//   //     unoptimized: true,
+//   // },
+//   handleImages: ['jpeg', 'png', 'svg'],
+// });
+
+// module.exports = {
+//   basePath: process.env.GITHUB_ACTIONS && "/nextjs-deploy-github-pages-test",
+//   trailingSlash: true,
+// //  assetPrefix: !debug ? "/notes/" : "",
+// //  images: { loader: "custom" },
+//   // experimental: {
+//   //   images: {
+//   //       unoptimized: true,
+//   //   },
+//   // },
+//   // images: {
+//   //     unoptimized: true,
+//   // },
+// };
+
 module.exports = {
   basePath: process.env.GITHUB_ACTIONS && "/nextjs-deploy-github-pages-test",
   trailingSlash: true,
-//  assetPrefix: !debug ? "/notes/" : "",
-//  images: { loader: "custom" },
-  // experimental: {
-  //   images: {
-  //       unoptimized: true,
-  //   },
-  // },
-  images: {
-      unoptimized: true,
-  },
+  assetPrefix: '/',
 };
